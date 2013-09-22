755dcf0199c28d51407dd26a9bc2d6de2f89e60a
Thil Bandara <tbandara@gmail.com>
2013-09-23 07:37:09 +1000

    initial test for movement

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -1,5 +1,6 @@
 require File.expand_path 'lib/robot'
 require File.expand_path 'lib/non_placement'
+require File.expand_path 'lib/pos'
 
 describe Robot do
 
@@ -22,6 +23,38 @@ describe Robot do
     context "invalid placement" do
       let(:valid) { false }
       it "should ignore placement" do
+        expect( subject.placement.report ).to be NonPlacement::REPORT
+      end
+    end
+  end
+
+  describe "#move" do
+    subject { robot.move; robot }
+
+    it "moves robot forward one position" do
+      robot.place(0, 0, POS::NORTH)
+      expect( subject.placement.y ).to be 1
+    end
+
+    it "moves robot forward one position" do
+      robot.place(0, 0, POS::EAST)
+      expect( subject.placement.x ).to be 1
+    end
+
+    context "on the edge of tabletop" do
+      it "restricts any movement off table vertically" do
+        robot.place(0, 0, POS::SOUTH)
+        expect( subject.placement.x ).to be 0
+      end
+
+      it "restricts any movement off table horizontally" do
+        robot.place(0, 0, POS::WEST)
+        expect( subject.placement.y ).to be 0
+      end
+    end
+
+    context "not placed" do
+      it "ignores movement" do
         expect( subject.placement.report ).to be NonPlacement::REPORT
       end
     end
